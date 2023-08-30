c7e96a2cf110a5621e577cc589afc5456bc8362f
“Reza” <reza_sahjpaul@hotmail.com>
2023-08-30 14:47:21 -0700

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let firstNumber = ''
+let operator = ''
+let secondNumber = ''
+
+
+let add = (a, b) => {
+  return a + b;
+};
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let divide = (a, b) => {
+  return a / b;
+};
+
+console.log(add(2,10));
+
+console.log(subtract(2,10));
+
+console.log(multiply(2,10));
+
+console.log(divide(2,10));
+
+
+
+
